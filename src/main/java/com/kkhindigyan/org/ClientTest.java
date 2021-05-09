637b66dc7e4d5b23369da9f367afa109ab921d4e
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-05-09 21:41:21 +0530

    java doc is updated in client program

--- src/main/java/com/kkhindigyan/org/ClientTest.java
+++ src/main/java/com/kkhindigyan/org/ClientTest.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.kkhindigyan.model.PaymentGateway;
 /**
  * @author Kishan Kumar
- *
+ *This is client program
  */
 public class ClientTest {
 
